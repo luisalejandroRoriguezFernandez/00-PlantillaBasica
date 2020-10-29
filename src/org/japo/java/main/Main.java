e7319dc99382582f2d56c8f0dbbc418a2042f75d
Luis <2001luisalejandro@gmail.com>
2020-10-29 09:41:25 +0100

    :tada: Commit Inicial2

--- src/org/japo/java/main/Main.java
+++ src/org/japo/java/main/Main.java
@@ -1,5 +1,5 @@
 /* 
- * Copyright 2020 alienware.
+ * Copyright 2020 Luis Alejandro Rodriguez - luisalejandro.rodirguez.alum@iescamp.es
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,7 +21,6 @@ package org.japo.java.main;
 public class Main {
     
     public static void main(String[] args) {
-        
  
     }
     
